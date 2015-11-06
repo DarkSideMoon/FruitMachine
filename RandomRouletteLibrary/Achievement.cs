559865b290db06c9874f5f1a68eb0905dca620be
DarkSideMoon <acubens96@meta.ua>
2015-11-07 01:13:28 +0200

    Change travis file2

--- RandomRouletteLibrary/Achievement.cs
+++ RandomRouletteLibrary/Achievement.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Media.Imaging;
 using System.Xml.Serialization;
 
 namespace RandomRouletteLibrary
